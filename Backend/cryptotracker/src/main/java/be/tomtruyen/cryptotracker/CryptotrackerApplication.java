c1b2a6db00a2ae4f89beddc964f5824d6edffbb7
Tom Truyen <tom.truyen@gmail.com>
2021-03-22 14:39:14 +0100

    Added start start code

---
+++ Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/CryptotrackerApplication.java
@@ -0,0 +1,13 @@
+package be.tomtruyen.cryptotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotrackerApplication.class, args);
+	}
+
+}
