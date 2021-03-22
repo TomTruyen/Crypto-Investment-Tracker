9f8c3879a0c0915bae3c25be8a459348b01a5acf
Tom Truyen <tom.truyen@gmail.com>
2021-03-22 19:01:56 +0100

    Added JDBC DatabaseService (MySQL)

---
+++ Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/services/DatabaseService.java
@@ -0,0 +1,81 @@
+package be.tomtruyen.cryptotracker.services;
+
+import be.tomtruyen.cryptotracker.domain.User;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class DatabaseService {
+    // useAffectedRows=true ==> Makes it so that when no rows are updated the updateCount = 0, otherwise if
+    // The query was still found it would return 1, because there was a matching result.
+    final private String database = "jdbc:mysql://localhost:3306/cryptotracker?useAffectedRows=true";
+    final private String user = "root";
+    final private String password = "";
+
+    public Connection connection;
+
+    public DatabaseService() throws SQLException {
+        this.connection = getConnection();
+    }
+
+    public Connection getConnection() throws SQLException{
+        Connection conn;
+
+        Properties properties = new Properties();
+        properties.put("user", user);
+        properties.put("password", password);
+
+        conn = DriverManager.getConnection(database, properties);
+
+        return conn;
+    }
+
+    public void closeConnection() throws SQLException {
+        if(connection != null) {
+            connection.close();
+        }
+    }
+
+    public User findUser(String email, String password) throws SQLException {
+        String query = "SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, password);
+
+        ResultSet rs = preparedStatement.executeQuery();
+
+        User user = null;
+        while(rs.next()) {
+            int id = rs.getInt("id");
+            boolean verified = rs.getBoolean("verified");
+
+            user = new User(id, email, password, verified);
+        }
+
+        return user;
+    }
+
+    public void addUser(String email, String password) throws SQLException {
+        String query = "INSERT INTO users (email, password, verified) VALUES (?, ?, ?)";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, email);
+        preparedStatement.setString(2, password);
+        preparedStatement.setBoolean(3, false);
+
+        preparedStatement.executeUpdate();
+    }
+
+    public boolean verifyUser(String email) throws SQLException {
+        String query = "UPDATE users SET verified = ? WHERE email = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setBoolean(1, true);
+        preparedStatement.setString(2, email);
+
+        preparedStatement.executeUpdate();
+
+        return preparedStatement.getUpdateCount() > 0;
+    }
+}
