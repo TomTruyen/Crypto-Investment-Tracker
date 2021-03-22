5fb6b430ffa23575e71a42be2cacb51de7e98453
Tom Truyen <tom.truyen@gmail.com>
2021-03-22 19:02:16 +0100

    Added Login

---
+++ Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/LoginController.java
@@ -0,0 +1,19 @@
+package be.tomtruyen.cryptotracker.rest;
+
+import be.tomtruyen.cryptotracker.services.LoginService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("login")
+public class LoginController {
+    @PostMapping
+    public ResponseEntity<Object> doLogin(@RequestBody Map<String, Object> body) {
+        return LoginService.login(body);
+    }
+}
