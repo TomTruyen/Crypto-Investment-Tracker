6f9a6ebd011e536e84146f3f1ccc7679d7a8a9bc
Tom Truyen <tom.truyen@gmail.com>
2021-03-23 13:29:06 +0100

    Added routes and basic login/register screen

---
+++ Frontend/cryptotracker/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <b-form>
+      <h3>Login</h3>
+      <hr />
+      <b-form-group label="Email address:" label-for="email">
+        <b-form-input id="email" type="text" placeholder="Email"></b-form-input>
+      </b-form-group>
+      <b-form-group label="Password:" label-for="password">
+        <b-form-input id="password" type="password" placeholder="Password"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Login</b-button>
+    </b-form>
+  </div>
+</template>
---
+++ Frontend/cryptotracker/src/views/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="register">
+    <b-form>
+      <h3>Register</h3>
+      <hr />
+      <b-form-group label="Email address:" label-for="email">
+        <b-form-input id="email" type="text" placeholder="Email"></b-form-input>
+      </b-form-group>
+      <b-form-group label="Password:" label-for="password">
+        <b-form-input id="password" type="password" placeholder="Password"></b-form-input>
+      </b-form-group>
+      <b-form-group label="Repeat password:" label-for="repeat-password">
+        <b-form-input id="repeat-password" type="password" placeholder="Repeat password"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Register</b-button>
+    </b-form>
+  </div>
+</template>
