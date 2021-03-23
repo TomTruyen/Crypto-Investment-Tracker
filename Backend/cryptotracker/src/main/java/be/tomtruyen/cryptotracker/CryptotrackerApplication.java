ed385ae49158fee2bac0939696970d714cacd5cc
Tom Truyen <tom.truyen@gmail.com>
2021-03-23 14:05:13 +0100

    Added CORS support

--- Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/CryptotrackerApplication.java
+++ Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/CryptotrackerApplication.java
@@ -2,11 +2,21 @@ package be.tomtruyen.cryptotracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CryptotrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CryptotrackerApplication.class, args);
+	}
+
+	@Configuration
+	static class WebMvcConfiguration implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
 	}
 }
