4cab702e6c8277f9e248a49d41e78d21453b0f4d
Tom Truyen <tom.truyen@gmail.com>
2021-03-23 16:26:36 +0100

    Added store

--- Frontend/cryptotracker/src/store/index.js
+++ Frontend/cryptotracker/src/store/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import API from './../api/Api.js';
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        isLoggedIn: false,
+        loginResult: null,
+    },
+    getters: {
+        getLogin(state) {
+            return state.loginResult;
+        },
+        isLoggedIn(state) {
+            return state.isLoggedIn;
+        },
+    },
+    mutations: {
+        login(state, loginResult) {
+            state.loginResult = loginResult;
+            if (loginResult.success && loginResult.token != "") state.isLoggedIn = true;
+        },
+        setIsLoggedIn(state, value) {
+            state.isLoggedIn = value;
+        }
+    },
+    actions: {
+        login(context, data) {
+            API.login(data['email'], data['password']).then((loginResult) => {
+                context.commit('login', loginResult);
+            });
+        }
+    }
 })
