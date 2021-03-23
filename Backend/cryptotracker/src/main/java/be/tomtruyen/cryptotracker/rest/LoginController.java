79927169ba1bcf4a9417c23d7f4580ae2c760897
Tom Truyen <tom.truyen@gmail.com>
2021-03-23 16:27:12 +0100

    Updated RequestMapping

--- Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/LoginController.java
+++ Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/LoginController.java
@@ -1,6 +1,7 @@
 package be.tomtruyen.cryptotracker.rest;
 
 import be.tomtruyen.cryptotracker.services.LoginService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("login")
+@RequestMapping(path="login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class LoginController {
     @PostMapping
     public ResponseEntity<Object> doLogin(@RequestBody Map<String, Object> body) {
