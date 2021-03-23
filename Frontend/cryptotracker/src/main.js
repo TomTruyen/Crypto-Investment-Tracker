db8b249692ea3be0e019937ec7315be434b5c27b
Tom Truyen <tom.truyen@gmail.com>
2021-03-23 16:26:31 +0100

    Added router check

--- Frontend/cryptotracker/src/main.js
+++ Frontend/cryptotracker/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import VueCookie from 'vue-cookie';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,8 +11,23 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
+Vue.use(VueCookie);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        if (!store.getters.isLoggedIn) {
+            next({ name: 'Login' })
+        } else {
+            next();
+        }
+    } else {
+        next() // does not require auth, make sure to always call next()!
+    }
+})
 
 new Vue({
     router,
