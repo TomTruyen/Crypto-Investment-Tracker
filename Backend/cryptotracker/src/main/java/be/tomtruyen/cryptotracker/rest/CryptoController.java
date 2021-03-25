f6759b5f53776ff8c270d1e16f7a0c9748e9f815
Tom Truyen <tom.truyen@gmail.com>
2021-03-25 16:59:40 +0100

    renamed path for cryptocurrencies

--- Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/CryptoController.java
+++ Backend/cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/CryptoController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path="crypto", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path="cryptocurrencies", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class CryptoController {
-    @GetMapping
     public ResponseEntity<Object> getCryptos(@RequestHeader Map<String, String> header) {
         return CryptoService.get(header);
     }
