1852a5bf971bd43fdeadeeb903e229f3686119d0
Tom Truyen <tom.truyen@gmail.com>
2021-03-29 20:43:06 +0200

    Added Java Frontend
    Decided to make a Windows Application using Java

--- Frontend/cryptotracker_old_flutter/lib/main.dart
+++ Frontend/cryptotracker_old_flutter/lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:cryptotracker/Wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'Wrapper.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,9 +21,6 @@ class MyApp extends StatelessWidget {
                 displayColor: Colors.white,
               ),
         ),
-        // textTheme: Theme.of(context).textTheme.apply(
-        //
-        //     ),
       ),
       initialRoute: '/',
       routes: {
