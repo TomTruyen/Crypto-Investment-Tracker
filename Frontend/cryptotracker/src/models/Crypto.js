78319705f729cd6c15b9b28f486a453a1c3a1e04
Tom Truyen <tom.truyen@gmail.com>
2021-04-11 12:58:26 +0200

    Added more attributes + fixed profitUSD function

--- Frontend/cryptotracker/src/models/Crypto.js
+++ Frontend/cryptotracker/src/models/Crypto.js
@@ -1,3 +1,5 @@
+import Utils from '@/utils/Utils.js';
+
 export default class Crypto {
     constructor(id, name, ticker, buyAmount, buyPrice, buyDate, sellAmount, sellPrice, sellDate) {
         this.id = id;
@@ -33,7 +35,7 @@ export default class Crypto {
     }
 
     calculateProfitUSD(amount, buyPrice, sellPrice) {
-        return Number(((amount * buyPrice) - (amount * sellPrice)).toFixed(2));
+        return Number(((amount * sellPrice) - (amount * buyPrice)).toFixed(2));
     }
 
     getBalance() {
@@ -46,23 +48,45 @@ export default class Crypto {
         return Number(this.getBalance() * currentPrice);
     }
 
-    toOption(currentPrice, change_24h) {
 
-        let price = currentPrice > 1 ? currentPrice.toFixed(2) : currentPrice.toFixed(6);
+
+
+    toOption(crypto) {
+        let rank = crypto.rank;
+        let marketCap = crypto.marketCap;
+        let currentPrice = crypto.price;
+        let change_24h = crypto.price_percent_change_24h;
+        let change_7d = crypto.price_percent_change_7d;
+        let change_30d = crypto.price_percent_change_30d;
+        let change_1y = crypto.price_percent_change_1y;
+        let ath = crypto.allTimeHigh;
+        let atl = crypto.allTimeLow;
+
+
         const balance = this.getBalance();
         let value = this.getValue(currentPrice);
         let profit = this.calculateProfit(currentPrice);
-        const profitUSD = this.calculateProfitUSD(balance, currentPrice, this.buyPrice);
+        const profitUSD = this.calculateProfitUSD(balance, this.buyPrice, currentPrice);
 
         return {
             "id": this.id,
             "name": `${this.name} (${this.ticker})`,
-            "price": `$${price}`,
-            "change_24h": `${change_24h.toFixed(2)}%`,
-            "balance": balance,
-            "value": `$${value.toFixed(2)}`,
-            "profit": `${profit.toFixed(2)}% ($${profitUSD.toFixed(2)})`,
+            "price": `$${Utils.numberWithCommas(currentPrice, currentPrice > 1 ? 2 : 6)}`,
+            "change_24h": `${Utils.numberWithCommas(change_24h)}%`,
+            "balance": Utils.numberWithCommas(balance),
+            "value": `$${Utils.numberWithCommas(value)}`,
+            "profit": `${Utils.numberWithCommas(profit)}% ($${Utils.numberWithCommas(profitUSD)})`,
             "ticker": this.ticker,
+            "details": {
+                "rank": `#${rank}`,
+                "marketCap": `$${Utils.numberWithCommas(marketCap)}`,
+                "change_24h": `${Utils.numberWithCommas(change_24h)}%`,
+                "change_7d": `${Utils.numberWithCommas(change_7d)}%`,
+                "change_30d": `${Utils.numberWithCommas(change_30d)}%`,
+                "change_1y": `${Utils.numberWithCommas(change_1y)}%`,
+                "ATH": `$${Utils.numberWithCommas(ath)}`,
+                "ATL": `$${Utils.numberWithCommas(atl)}`,
+            },
         };
     }
 
