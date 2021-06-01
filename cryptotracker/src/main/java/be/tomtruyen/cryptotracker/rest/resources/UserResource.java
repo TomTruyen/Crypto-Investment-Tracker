8f7efd3a3cae44d7786be36161fdbff27edfe3d6
TomTruyen <tom.truyen@gmail.com>
2021-06-01 15:25:44 +0200

    Add Email Pattern validation

--- cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/resources/UserResource.java
+++ cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/resources/UserResource.java
@@ -1,11 +1,13 @@
 package be.tomtruyen.cryptotracker.rest.resources;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class UserResource {
-    @NotBlank
+    @NotEmpty(message = "Email can't be empty")
+    @Pattern(regexp = "\\w+@\\w+\\.\\w+(,\\s*\\w+@\\w+\\.\\w+)*", message = "Email is invalid")
     private String email;
-    @NotBlank
+    @NotEmpty(message = "Password can't be empty")
     private String password;
 
     public String getEmail() {
