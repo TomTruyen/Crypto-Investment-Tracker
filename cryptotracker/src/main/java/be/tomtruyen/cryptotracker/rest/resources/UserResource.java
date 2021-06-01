915abad43cb836b85a1c51ebceef5f2ddfb8ebe7
TomTruyen <tom.truyen@gmail.com>
2021-06-01 21:02:20 +0200

    Fix email validation

--- cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/resources/UserResource.java
+++ cryptotracker/src/main/java/be/tomtruyen/cryptotracker/rest/resources/UserResource.java
@@ -1,11 +1,11 @@
 package be.tomtruyen.cryptotracker.rest.resources;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 
 public class UserResource {
     @NotEmpty(message = "Email can't be empty")
-    @Pattern(regexp = "\\w+@\\w+\\.\\w+(,\\s*\\w+@\\w+\\.\\w+)*", message = "Email is invalid")
+    @Email(message = "Email is invalid")
     private String email;
     @NotEmpty(message = "Password can't be empty")
     private String password;
