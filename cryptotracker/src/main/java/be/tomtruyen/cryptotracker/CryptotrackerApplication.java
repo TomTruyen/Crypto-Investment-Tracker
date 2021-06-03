e12243a2142db651053bd5dd43a20ee1593f1286
TomTruyen <tom.truyen@gmail.com>
2021-06-03 12:56:41 +0200

    Add logging to file

--- cryptotracker/src/main/java/be/tomtruyen/cryptotracker/CryptotrackerApplication.java
+++ cryptotracker/src/main/java/be/tomtruyen/cryptotracker/CryptotrackerApplication.java
@@ -1,12 +1,18 @@
 package be.tomtruyen.cryptotracker;
 
+import be.tomtruyen.cryptotracker.rest.handler.JpaExceptionHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CryptotrackerApplication {
+	private static final Logger LOGGER = LogManager.getLogger(CryptotrackerApplication.class);
 
 	public static void main(String[] args) {
+		LOGGER.info("Starting Application.");
+
 		SpringApplication.run(CryptotrackerApplication.class, args);
 	}
 
